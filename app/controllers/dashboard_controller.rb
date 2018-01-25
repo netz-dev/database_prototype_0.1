9f7610eb0ae13a0316ebb79b1af1d7ff6491f5eb
hirespan <nick@netz.uk>
2018-01-25 14:14:09 +1300

    Minor improvements.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < ApplicationController
   layout "layouts/dashboard/application"
   def index
+    @total_clients = Client.all
+    @live_clients = Client.all
+    @live_campaigns = Campaign.all
+    @total_prospects = Prospect.all
+    @prospects_in_sequence = Prospect.all
   end
 end
